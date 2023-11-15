b4c9623bd97a08afa8bb1c5256115ee19b8e3ae4
tran-duc-luong <150908236+tran-duc-luong@users.noreply.github.com>
2023-11-15 19:45:32 +0700

    Add files via upload

---
+++ song.js
@@ -0,0 +1,8 @@
+let songs = [
+    {
+        name:'Một đời',
+        path:'music/alo.mp3',
+        artist:'14 Casper & Bon Nghiêm',
+        image:'images/z4883183793329_24432820338818220d0acafd3183721d.jpg'
+    }
+]
